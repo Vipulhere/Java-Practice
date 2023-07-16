7981bf0da7337a2ee58addf98f756b7c94c3f4a2
Vipulhere <krvipul28@gmail.com>
2023-07-17 00:59:04 +0530

    17July2023

---
+++ FindElementInArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class FindElementInArray {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner (System.in);
+        int n=scn.nextInt();
+        int arr []=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=scn.nextInt();
+        }
+
+        int data=scn.nextInt();
+        int index=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==data){
+                index=i;
+                break;
+            }
+        }
+        System.out.println(index);
+    }    
+}
